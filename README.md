d2f95d9d413761011b12e37b39f15ae561dd104f
Savi93 <save.1993@gmail.com>
2023-04-25 21:38:34 +0200

    Second PWM output added with H-bridge controller

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # DC motor controller with Atmega2560
 
-The following firmware was developed for an Atmega2560 µC (Atmel) by using the C synthax and permits the control of a DC motor through two pushbuttons (one for incrementing and the other for decrementing the RPMs of the electro-mechanical device).
+The following firmware was developed for an Atmega2560 µC (Atmel - AVR architecture) by using the C synthax and permits the control of a DC motor through tree pushbuttons (two for incrementing (PB0) and decrementing (PB1) the RPMs of the electro-mechanical device, while the third for inverting the rotation sense).
 
 The methodology of management was done by adopting the "Fast PWM" control technique with resolution 2^10bits (0-1023 steps).
-This means that a digital output pin was configured to act in a switching mode to properly control the motor.
+This means that two different digital output pins (PB5 and PB6) were configured to act in a switching mode to properly control the motor according to requested rotation sense.
 The scaling was however changed in order to have a proper association of the physical clicks of the pushbuttons with a percentage value ranging from 0 (motor OFF) to 100% (max speed).<br>
 ```c
 #define TOP_VAL 1023
@@ -12,7 +12,7 @@ uint16_t res = 0;
 res = ((TOP_VAL / 100.0)) * duty;
 ``` 
 
-A mosfet transistor was placed as intermediate component between microcontroller and motor in order to deal with the current limitations of the output ports of the Atmega.
+An H-Bridge composed by 4 mosfet was placed as intermediate component between microcontroller and motor in order to deal with the current limitations of the output ports of the Atmega and to correctly manage the current flow through the device.
 
 **Extra's**: The implementation was successfully tested with SimulIde simulation environment.
 
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 /*
  * Fast_PWM.c
+ *
  * Created: 09/03/2022 20:28:33
  * Author : Savii
  */ 
@@ -7,14 +8,20 @@
 #define F_CPU 16000000
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
 
 #define TOP_VAL 1023.0
+#define TOGGLE(X) X = (X ^ 0x01)
+#define LEFT 1
+#define RIGHT 0
 
 volatile uint8_t* MEM_PORTB = (uint8_t*) (0x25);
 volatile uint8_t* MEM_DDRB = (uint8_t*) (0x24);
 volatile uint8_t* MEM_PINB = (uint8_t*) (0x23);
 
 volatile uint8_t count = 0;
+volatile uint8_t direct = RIGHT;
 
 inline uint16_t dutyToCount(uint8_t duty)
 {
@@ -22,39 +29,54 @@ inline uint16_t dutyToCount(uint8_t duty)
 	res = ((TOP_VAL / 100.0)) * duty;
 	return res;
 }
+
 inline void updateDuty(uint8_t duty)
 {
 	if(duty <= 100)
-	{	
-		OCR1AL = dutyToCount(duty);
-		OCR1AH = dutyToCount(duty) >> 8;
+	{
+		switch(direct)
+		{
+			case RIGHT:
+				OCR1AL = (duty > 0) ? dutyToCount(duty) : 0;
+				OCR1AH = (duty > 0) ? dutyToCount(duty) >> 8 : 0;
+				break;
+			case LEFT:
+				OCR1BL = (duty > 0) ? dutyToCount(duty) : 0;
+				OCR1BH = (duty > 0) ? dutyToCount(duty) >> 8 : 0;
+				break;
+			default:
+				break;
+		}
 	}
 }
 
 inline void startPWMTimer()
 { 
-	TCNT1L = 0;
-	TCNT1H = 0;
-	TCCR1A = (1<<WGM10) | (1<<WGM11);
-	TCCR1B = (1<<WGM12); //This operation and the previous selects FAST PWM with resolution 10bits (1023 is TOP val)
-	TCCR1A |= (1<<COM1A1); //Clear on compare match, set at BOTTOM
+	TCNT1L = 0x00;
+	TCNT1H = 0x00;
+	TCCR1A = (direct == RIGHT) ? (1<<COM1A1) : (1<<COM1B1); //Clear on compare match, set at BOTTOM
 	TCCR1B |= (1<<CS11); //CLK prescaler division by 8; 16M / 8 = 2M; PWM signal = 2M / 1024 = 1953,125Hz
+	TCCR1A |= (1<<WGM10) | (1<<WGM11);
+	TCCR1B |= (1<<WGM12); //This operation and the previous selects FAST PWM with resolution 10bits (1023 is TOP val)
 	updateDuty(0); //Set PWM duty = 0
 	TIMSK1 = (1<<TOIE1); //Enable timer overflow interrupt
 }
 
 inline void stopPWMTimer()
 {
-	TCCR1B &= ~(1<<CS11); //Prescaler = 0; stop timer
+	TCNT1H = 0x00; //Reset count value
+	TCNT1L = 0x00; //Reset count value
 	updateDuty(0); //Reset PWM duty
-	TCNT1L = 0; //Reset count value
-	TCNT1H = 0; //Reset count value
+	_delay_ms(1); //Delay needed to reset permit the clear on compare match operation
+	TCCR1B &= 0x00; //Prescaler = 0; stop timer
+	TCCR1A &= 0x00;
+	TIMSK1 &= 0x00; //Disable timer interrupts
 }
 
 inline void initButtonsINT()
 {
-	PCICR = (1<<PCIE0); //Enable Pin Change INT0 (Buttons are on PB0 and PB1)
-	PCMSK0 = (1<<PCINT0) | (1<<PCINT1); //Un-mask INT on pin 0 and pin 1 
+	PCICR = (1<<PCIE0); //Enable Pin Change INT0 (Buttons are on PB0, PB1 and PB2)
+	PCMSK0 = (1<<PCINT0) | (1<<PCINT1) | (1<<PCINT2); //Unmasking INT on pin 0, pin 1 and pin 2
 }
 
 ISR(TIMER1_OVF_vect)
@@ -64,24 +86,43 @@ ISR(TIMER1_OVF_vect)
 
 ISR(PCINT0_vect)
 {
-	if((*(MEM_PINB) & 1) == 0 && count < 100)
+	//Increment pushbutton (PB0)
+	if((*(MEM_PINB) & 0x01) == 0 && count < 100)
 		count++;
-	else if((*(MEM_PINB) & 2) == 0 && count > 0)
+	//Decrement pushbutton (PB1)
+	else if((*(MEM_PINB) & 0x02) == 0 && count > 0)
 		count--;
+	//Invert pushbutton (PB2)
+	else if((*(MEM_PINB) & 0x04) == 0)
+	{
+		count = 0;
+		
+		stopPWMTimer();
+		direct = TOGGLE(direct);
+		
+		startPWMTimer();
+	}
+	
+	*(MEM_PORTB) &= 0b01100111;
+	*(MEM_PORTB) |= (direct == RIGHT) ? 0b00001000 : 0b00010000;
 }
 
 int main()
 {
-	*(MEM_PORTB) |= 0b111; //Enable pull-up resistors on PB0, PB1, PB2
+	*(MEM_PORTB) |= 0b00000111; //Enable pull-up resistors on PB0, PB1, PB2
+	*(MEM_DDRB) |= 0b00011000; //PB3 and PB4 enabled as output
 	
 	initButtonsINT();
 	startPWMTimer();
+	
+	*(MEM_PORTB) |= (direct == RIGHT) ? 0b00001000 : 0b00010000; //Initialize first rotation signalization LED
+	
 	sei();
 	
     /* Replace with your application code */
-    while (1) 
+    while(1) 
     {
-
+		
     }
 }
 
